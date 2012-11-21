91fbe726f0ada4e45480f42338a03de89b12c657
Carla Berteil <carla.brtl@gmail.com>
2012-11-21 13:01:41 +0100

    Assignment 2 suite

--- GearEditor/GearBoxGenerator.cs
+++ GearEditor/GearBoxGenerator.cs
@@ -49,6 +49,7 @@ namespace GearEditor
                 GearBox gearBox = new GearBox();
                 Gear inputGear = new Gear();
                 Gear outputGear = new Gear();
+                double alpha = Convert.ToDouble(numAlpha.Value)*Math.PI/180;
 
                 /*
                  * Data extraction 
@@ -56,7 +57,7 @@ namespace GearEditor
 
                 // Gear Box Creation
                 gearBox.Name = txtName.Text;
-                gearBox.Alpha = Convert.ToDouble(numAlpha.Value);
+                gearBox.Alpha = alpha;
                 gearBox.GearModule = Convert.ToDouble(numGearModule.Value);
                 gearBox.AxesDistance = axesDistance;
                 gearBox.TorqueRatio = torqueRatio;
@@ -65,7 +66,7 @@ namespace GearEditor
 
                 // Input Gear Creation
                 inputGear.Name = txtName.Text + " Input Gear";
-                inputGear.Alpha = Convert.ToDouble(numAlpha.Value);
+                inputGear.Alpha = alpha;
                 inputGear.GearModule = Convert.ToDouble(numGearModule.Value);
                 inputGear.WantedRadius = axesDistance * (1 / (1 + torqueRatio));
                 inputGear.Torque = Convert.ToDouble(numInputTorque.Value);
@@ -74,7 +75,7 @@ namespace GearEditor
 
                 // Output Gear Creation
                 outputGear.Name = txtName.Text + " Output Gear";
-                outputGear.Alpha = Convert.ToDouble(numAlpha.Value);
+                outputGear.Alpha = alpha;
                 outputGear.GearModule = Convert.ToDouble(numGearModule.Value);
                 outputGear.WantedRadius = axesDistance * (1 - 1 / (1 + torqueRatio));
                 outputGear.Torque = Convert.ToDouble(numOutputTorque.Value);
@@ -86,6 +87,10 @@ namespace GearEditor
 
                 //Input Gear Calculation
                 gearCalculation(inputGear, true);
+
+                updateGearOnCatia(inputGear, true);
+                updateGearOnCatia(outputGear, false);
+          //      showGearboxOnCatia();
 
                 this.btnValidate.Visible = true;
             }
@@ -231,6 +236,102 @@ namespace GearEditor
                 btnOutputMaterial.Size = buttonSize;
                 btnOutputMaterial.Location = new Point(213, 196);
             }
+        }
+
+        private void updateGearOnCatia(Gear g, Boolean isInput) {
+            INFITF.Application catiaApp = null;
+            try
+            {
+                catiaApp = System.Runtime.InteropServices.Marshal.GetActiveObject("CATIA.Application") as INFITF.Application;
+            }
+            catch (Exception ex)
+            {
+                catiaApp = System.Activator.CreateInstance(Type.GetTypeFromProgID("CATIA.Application")) as INFITF.Application;
+            }
+            finally
+            {
+                catiaApp.Visible = true;
+                if (isInput == true)
+                {
+                    catiaApp.Documents.Open(path + "\\input_gear.CATPart");
+                }
+                else
+                    catiaApp.Documents.Open(path + "\\output_gear.CATPart");
+            }
+            MECMOD.PartDocument partDocument = (MECMOD.PartDocument) catiaApp.ActiveDocument;  
+            ProductStructureTypeLib.Product product = partDocument.Product;
+
+            KnowledgewareTypeLib.Parameter type = (KnowledgewareTypeLib.Parameter) product.Parameters.GetItem("Alpha");
+            type.ValuateFromString(g.Alpha + "rad");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("Module");
+            type.ValuateFromString(g.GearModule + "mm");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("Z");
+            type.ValuateFromString(g.Z.ToString());
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("Radius");
+            type.ValuateFromString(g.WantedRadius + "mm");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("r_top");
+            type.ValuateFromString(g.R_top+ "mm");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("r_base");
+            type.ValuateFromString(g.R_pitch + "mm");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("r_foot");
+            type.ValuateFromString(g.R_foot + "mm");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("phi");
+            type.ValuateFromString((g.Phi*Math.PI/180) + "rad");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("D_Shaft");
+            type.ValuateFromString(g.Shaft.Diameter + "mm");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("KeyWidth");
+            type.ValuateFromString(g.Shaft.KeyCutWidth + "mm");
+            
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("KeyLength");
+            type.ValuateFromString(g.Shaft.KeyCutLength + "mm");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("KeyWidth");
+            type.ValuateFromString(g.Shaft.KeyCutWidth + "mm");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("C1");
+            if (isInput == true)
+                type.ValuateFromString(inputC1 + "mm");
+            else 
+                type.ValuateFromString(outputC1 + "mm");
+
+            type = (KnowledgewareTypeLib.Parameter)product.Parameters.GetItem("C2");
+            if (isInput == true)
+                type.ValuateFromString(inputC2 + "mm");
+            else 
+                type.ValuateFromString(outputC2 + "mm");
+
+            product.Update();
+            partDocument.Save(); 
+
+        }
+
+        public void showGearboxOnCatia() {
+            INFITF.Application catiaApp = null;
+            try
+            {
+                catiaApp = System.Runtime.InteropServices.Marshal.GetActiveObject("CATIA.Application") as INFITF.Application;
+            }
+            catch (Exception ex)
+            {
+                catiaApp = System.Activator.CreateInstance(Type.GetTypeFromProgID("CATIA.Application")) as INFITF.Application;
+            }
+            finally
+            {
+                catiaApp.Visible = true;
+                catiaApp.Documents.Open(path + "\\input_gear.CATPart");
+            }
+            MECMOD.PartDocument partDocument = (MECMOD.PartDocument)catiaApp.ActiveDocument;
+            ProductStructureTypeLib.Product product = partDocument.Product;
+
         }
     }
 }
